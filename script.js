08d18acf887194355245b4108d24d6dd8c016031
pmorais13 <phmscout@gmail.com>
2022-08-08 23:32:40 -0300

    feat: ajuste no carregamento do audio

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const audios = {
     belezita: './src/audio/belezita.wav'
 }
 
+function initAudios() {
+    const soms = Object.keys(audios)
+    debugger
+    soms.map((som) => {
+        let audio = new Audio(audios[som]);
+        audio.volume = 0;
+        audio.load();
+        console.log(som);
+    })
+}
+
 function playAudio(varAudio) {   
     let audio = new Audio(audios[varAudio]);
     audio.play();
 }
 
+initAudios()
